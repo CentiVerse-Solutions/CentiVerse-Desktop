7bbb30160bebfa99cfa4287c08baffda514b7809
Harshit-Chordiya <harshitchordiya03@gmail.com>
2025-03-05 18:43:52 +0530

    modifications in auth

---
+++ src-tauri/src/entities/activities.rs
@@ -0,0 +1,59 @@
+use sea_orm::entity::prelude::*;
+use serde::{Deserialize, Serialize};
+use rust_decimal::Decimal;
+
+#[derive(Clone, Debug, PartialEq, DeriveEntityModel, Serialize, Deserialize)]
+#[sea_orm(table_name = "activities")]
+pub struct Model {
+    #[sea_orm(primary_key)]
+    pub id: String,
+    pub description: Option<String>,
+    pub paid_by_id: String,
+    pub group_id: String,
+    pub time: Option<DateTime>,
+    pub amount: Option<Decimal>,
+    pub split_members: Option<Vec<String>>,
+    pub split_amounts: Option<Vec<Decimal>>,
+    pub user_involvement: Option<bool>,
+    pub expense_logo: Option<String>,
+    pub created_at: Option<DateTime>,
+    pub updated_at: Option<DateTime>,
+}
+
+#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+pub enum Relation {
+    #[sea_orm(
+        belongs_to = "super::users::Entity",
+        from = "Column::PaidById",
+        to = "super::users::Column::Id"
+    )]
+    PaidBy,
+    #[sea_orm(
+        belongs_to = "super::groups::Entity",
+        from = "Column::GroupId",
+        to = "super::groups::Column::Id"
+    )]
+    Group,
+    #[sea_orm(has_many = "super::transactions::Entity")]
+    Transactions,
+}
+
+impl Related<super::users::Entity> for Entity {
+    fn to() -> RelationDef {
+        Relation::PaidBy.def()
+    }
+}
+
+impl Related<super::groups::Entity> for Entity {
+    fn to() -> RelationDef {
+        Relation::Group.def()
+    }
+}
+
+impl Related<super::transactions::Entity> for Entity {
+    fn to() -> RelationDef {
+        Relation::Transactions.def()
+    }
+}
+
+impl ActiveModelBehavior for ActiveModel {}
--- src-tauri/src/routes/auth.rs
+++ src-tauri/src/routes/auth.rs
@@ -1,10 +1,10 @@
-// src/routes/auth.rs
-
-use axum::routing::get;
+use axum::routing::{get, post};
 use axum::Router;
-use crate::controllers::auth_controller::login_handler;
+use crate::controllers::auth_controller::{login_handler, signup_handler};
 
 /// Constructs the auth routes.
 pub fn router() -> Router {
-    Router::new().route("/auth/login", get(login_handler))
+    Router::new()
+        .route("/auth/login", get(login_handler))
+        .route("/auth/signup", post(signup_handler))
 }
