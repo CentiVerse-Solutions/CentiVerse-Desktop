15d65d3790f0bc15f61bb09f7715b640116963c8
[karthik] <[lcs2022026@iiitl.ac.in]>
2025-03-25 19:14:39 +0530

    models::groups : added and impl GroupRes

--- src-tauri/src/models/groups.rs
+++ src-tauri/src/models/groups.rs
@@ -28,3 +28,50 @@ impl CreateGroupReq{
         }
     }
 }
+
+#[derive(Debug, Clone, PartialEq,Serialize)]
+pub struct GroupRes{
+    pub id: Uuid,
+    pub creator_id: Uuid,
+    pub group_name: String,
+    pub auto_logo: Option<String>,
+    pub total_expense: Decimal,
+    pub created_at: DateTimeWithTimeZone,
+    pub updated_at: DateTimeWithTimeZone,
+}
+
+impl GroupRes{
+    pub fn new(
+        id: Uuid,
+        creator_id: Uuid,
+        group_name: String,
+        auto_logo: Option<String>,
+        total_expense: Decimal,
+        created_at: DateTimeWithTimeZone,
+        updated_at: DateTimeWithTimeZone,
+    ) -> Self{
+        Self{
+            id,
+            creator_id,
+            group_name,
+            auto_logo,
+            total_expense,
+            created_at,
+            updated_at,
+        }
+    }
+}
+
+impl From<crate::entities::groups::Model> for GroupRes{
+    fn from(group: crate::entities::groups::Model) -> Self {
+        Self::new(
+            group.id,
+            group.creator_id,
+            group.group_name,
+            group.auto_logo,
+            group.total_expense,
+            group.created_at,
+            group.updated_at,
+        )
+    }
+}
--- src-tauri/src/request_verifier/users.rs
+++ src-tauri/src/request_verifier/users.rs
@@ -13,7 +13,7 @@ use crate::custom_errors::auth::AuthError;
 use dotenv::dotenv;
 use std::env;
 
-pub async fn verify_user<B>(
+pub async fn re<B>(
     Extension(db): Extension<DatabaseConnection>,
     mut req: Request<B>,
     next: Next<B>,
