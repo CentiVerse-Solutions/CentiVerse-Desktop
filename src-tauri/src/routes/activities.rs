6d0855391d4644e9c39b4013c519a20779be931d
Harshit-Chordiya <harshitchordiya03@gmail.com>
2025-04-02 11:35:09 +0530

    activities_controller:added get_all_activities_controller

--- src-tauri/src/routes/activities.rs
+++ src-tauri/src/routes/activities.rs
@@ -1,10 +1,11 @@
 use axum::{routing::{get, post,patch,delete}, Router, middleware};
-use crate::controllers::activities_controller::{create_activity_handler,update_activity_handler,delete_activity_handler};
+use crate::controllers::activities_controller::{create_activity_handler,update_activity_handler,delete_activity_handler,get_all_activities_handler};
 use crate::request_verifier::{users::verify_user};
 pub fn router() -> Router {
     Router::new()
         .route("/activities/create_activities", post(create_activity_handler))
         .route("/activities/update_activities", patch(update_activity_handler))
         .route("/activities/delete_activities", delete(delete_activity_handler))
+        .route("/activities/get_all_activities", get(get_all_activities_handler))
         .layer(middleware::from_fn(verify_user))
 }
