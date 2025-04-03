a555a5b7ba37c6d4b694d691d3bef3414a10858e
Harshit-Chordiya <harshitchordiya03@gmail.com>
2025-04-03 12:04:53 +0530

    group_members_controller:added remove_group_member

--- src-tauri/src/routes/activities.rs
+++ src-tauri/src/routes/activities.rs
@@ -1,6 +1,6 @@
 use axum::{routing::{get, post,patch,delete}, Router, middleware};
 use crate::controllers::activities_controller::{create_activity_handler,update_activity_handler,delete_activity_handler,get_all_activities_handler};
-use crate::request_verifier::{users::verify_user};
+use crate::request_verifier::users::verify_user;
 pub fn router() -> Router {
     Router::new()
         .route("/activities/create_activities", post(create_activity_handler))
